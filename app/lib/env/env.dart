29f2cb385f8dc900cbb3414581f172e2b90dbfff
huangjianjin <49916645+hjj88@users.noreply.github.com>
2025-12-10 18:52:04 +0800

    tmp

--- app/lib/env/env.dart
+++ app/lib/env/env.dart
@@ -13,8 +13,9 @@ abstract class Env {
 
   // static String? get apiBaseUrl => 'https://omi-backend.ngrok.app/';
 
-  // static String? get apiBaseUrl => _instance.apiBaseUrl;
-  static String? get apiBaseUrl => 'https://memopin.ai/';
+  // static String? get apiBaseUrl => _instance.apiBaseUrl; // omi
+
+  static String? get apiBaseUrl => 'https://memopin.ai/'; // mp
 
   static String? get noteBaseUrl => _instance.noteBaseUrl;
 
