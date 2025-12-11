901171aa9afc5ec0060b01ae897b2425f9cdd487
huang pang <huang@huangdeMac-mini.local>
2025-12-11 23:10:16 +0800

    tmp

--- app/lib/pages/mp_memo_todo/todo/todo_page.dart
+++ app/lib/pages/mp_memo_todo/todo/todo_page.dart
@@ -1,6 +1,6 @@
 // AI-generated START - Todo 页面
 import 'package:flutter/material.dart';
-import 'package:omi/pages/memo_todo/todo/widgets/todo_task_card.dart';
+import 'widgets/todo_task_card.dart';
 import 'package:omi/pages/mp_memo_todo/home/widgets/search_tasks_card.dart';
 import 'package:omi/pages/mp_memo_todo/todo/providers/todo_provider.dart';
 import 'package:provider/provider.dart';
