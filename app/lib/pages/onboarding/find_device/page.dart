93d4abab89c2f3caaf86e0dc432bec6135ffd3ba
huang pang <huang@huangdeMac-mini.local>
2025-12-15 06:44:05 +0800

    tmp

--- app/lib/pages/onboarding/find_device/page.dart
+++ app/lib/pages/onboarding/find_device/page.dart
@@ -61,7 +61,7 @@ class _FindDevicesPageState extends State<FindDevicesPage> {
               },
               () {},
               'Enable Bluetooth',
-              'Omi needs Bluetooth to connect to your wearable. Please enable Bluetooth and try again.',
+              'MemoPin needs Bluetooth to connect to your wearable. Please enable Bluetooth and try again.',
               singleButton: true,
             ),
           );
