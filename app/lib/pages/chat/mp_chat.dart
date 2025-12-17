c869480a257eac665647e4e1c4b30643ee1a905b
huang pang <huang@huangdeMac-mini.local>
2025-12-17 22:36:44 +0800

    tmp

--- app/lib/pages/chat/mp_chat.dart
+++ app/lib/pages/chat/mp_chat.dart
@@ -33,6 +33,20 @@ import 'package:share_plus/share_plus.dart';
 import 'widgets/message_action_menu.dart';
 import 'widgets/mp_chat_appbar.dart';
 
+// 聊天页面类型。不同类型调用url接口入参不同。
+enum MPChatPageType {
+  // 普通聊天
+  normal,
+  // 记忆总结
+  memory,
+  // 模板聊天
+  template,
+  // AI分析助手
+  aiAssistant,
+  // 专家模型
+  expert,
+}
+
 class MPChatPage extends StatefulWidget {
   final bool isPivotBottom;
 
@@ -101,15 +115,15 @@ class MPChatPageState extends State<MPChatPage> with AutomaticKeepAliveClientMix
       if (provider.messages.isEmpty) {
         provider.refreshMessages();
       }
-      provider.fetchChatApps();
+      // provider.fetchChatApps();
       scrollToBottom();
-      if (_isInitialLoad) {
-        Future.delayed(const Duration(milliseconds: 300), () {
-          if (mounted && !_showVoiceRecorder && _isInitialLoad) {
-            textFieldFocusNode.requestFocus();
-          }
-        });
-      }
+      // if (_isInitialLoad) {
+      //   Future.delayed(const Duration(milliseconds: 300), () {
+      //     if (mounted && !_showVoiceRecorder && _isInitialLoad) {
+      //       textFieldFocusNode.requestFocus();
+      //     }
+      //   });
+      // }
     });
     super.initState();
   }
@@ -131,7 +145,6 @@ class MPChatPageState extends State<MPChatPage> with AutomaticKeepAliveClientMix
         return Scaffold(
           key: scaffoldKey,
           backgroundColor: Theme.of(context).colorScheme.primary,
-          // appBar: _buildAppBar(context, provider),
           appBar: MPChatAppBar(
             onLeftIconTap: () => (),
             onMenuTap: () => (),
@@ -332,12 +345,13 @@ class MPChatPageState extends State<MPChatPage> with AutomaticKeepAliveClientMix
                                 ),
                 ),
                 Container(
-                  margin: const EdgeInsets.only(top: 10),
-                  decoration: const BoxDecoration(
-                    color: Color(0xFF1f1f25),
-                    borderRadius: BorderRadius.only(
-                      topLeft: Radius.circular(22),
-                      topRight: Radius.circular(22),
+                  margin: const EdgeInsets.only(top: 10, left: 16, right: 16),
+                  decoration:  BoxDecoration(
+                    color: Colors.white,
+                    borderRadius:  BorderRadius.circular(22),
+                    border: Border.all(
+                      color:const Color(0xFFE5E7EB),
+                      width: 1.0,
                     ),
                   ),
                   child: Consumer<HomeProvider>(builder: (context, home, child) {
@@ -349,102 +363,13 @@ class MPChatPageState extends State<MPChatPage> with AutomaticKeepAliveClientMix
                       return !_showVoiceRecorder;
                     }
 
-                    bool shouldShowMenuButton() {
-                      return !_showVoiceRecorder;
-                    }
-
                     return Column(
                       children: [
                         Consumer<MessageProvider>(builder: (context, provider, child) {
-                          if (provider.selectedFiles.isNotEmpty) {
-                            return Container(
-                              margin: const EdgeInsets.only(top: 16, bottom: 8),
-                              padding: const EdgeInsets.symmetric(horizontal: 16),
-                              height: 70,
-                              child: ListView.builder(
-                                scrollDirection: Axis.horizontal,
-                                itemCount: provider.selectedFiles.length,
-                                itemBuilder: (ctx, idx) {
-                                  return Container(
-                                    margin: const EdgeInsets.only(right: 8),
-                                    width: 60,
-                                    height: 60,
-                                    decoration: BoxDecoration(
-                                      color: Colors.grey[800],
-                                      borderRadius: BorderRadius.circular(16),
-                                      image: provider.selectedFileTypes[idx] == 'image'
-                                          ? DecorationImage(
-                                              image: FileImage(provider.selectedFiles[idx]),
-                                              fit: BoxFit.cover,
-                                            )
-                                          : null,
-                                    ),
-                                    child: Stack(
-                                      children: [
-                                        if (provider.selectedFileTypes[idx] != 'image')
-                                          const Center(
-                                            child: Icon(
-                                              Icons.insert_drive_file,
-                                              color: Colors.white,
-                                              size: 24,
-                                            ),
-                                          ),
-                                        if (provider.isFileUploading(provider.selectedFiles[idx].path))
-                                          Container(
-                                            decoration: BoxDecoration(
-                                              color: Colors.black.withOpacity(0.5),
-                                              borderRadius: BorderRadius.circular(16),
-                                            ),
-                                            child: const Center(
-                                              child: SizedBox(
-                                                width: 16,
-                                                height: 16,
-                                                child: CircularProgressIndicator(
-                                                  strokeWidth: 2,
-                                                  valueColor: AlwaysStoppedAnimation<Color>(Colors.white70),
-                                                ),
-                                              ),
-                                            ),
-                                          ),
-                                        Positioned(
-                                          top: 4,
-                                          right: 4,
-                                          child: GestureDetector(
-                                            onTap: () {
-                                              provider.clearSelectedFile(idx);
-                                            },
-                                            child: Container(
-                                              width: 16,
-                                              height: 16,
-                                              decoration: BoxDecoration(
-                                                color: Colors.white,
-                                                borderRadius: BorderRadius.circular(10),
-                                              ),
-                                              child: const Icon(
-                                                FontAwesomeIcons.xmark,
-                                                size: 10,
-                                                color: Colors.black,
-                                              ),
-                                            ),
-                                          ),
-                                        ),
-                                      ],
-                                    ),
-                                  );
-                                },
-                              ),
-                            );
-                          } else {
                             return const SizedBox.shrink();
-                          }
                         }),
                         Padding(
-                          padding: EdgeInsets.only(
-                            left: 0,
-                            right: 16,
-                            top: provider.selectedFiles.isNotEmpty ? 0 : 16,
-                            bottom: widget.isPivotBottom ? 20 : (textFieldFocusNode.hasFocus ? 20 : 40),
-                          ),
+                          padding: const EdgeInsets.all(8),
                           child: Row(
                             crossAxisAlignment: CrossAxisAlignment.end,
                             children: [
@@ -454,33 +379,6 @@ class MPChatPageState extends State<MPChatPage> with AutomaticKeepAliveClientMix
                                   child: Row(
                                     crossAxisAlignment: CrossAxisAlignment.end,
                                     children: [
-                                      if (shouldShowMenuButton())
-                                        GestureDetector(
-                                          onTap: () {
-                                            FocusScope.of(context).unfocus();
-                                            if (provider.selectedFiles.length > 3) {
-                                              ScaffoldMessenger.of(context).showSnackBar(
-                                                const SnackBar(
-                                                  content: Text('You can only upload 4 files at a time'),
-                                                  duration: Duration(seconds: 2),
-                                                ),
-                                              );
-                                              return;
-                                            }
-                                            _showIOSStyleActionSheet(context);
-                                          },
-                                          child: Container(
-                                            margin: const EdgeInsets.only(right: 4),
-                                            height: 44,
-                                            width: 44,
-                                            alignment: Alignment.center,
-                                            child: FaIcon(
-                                              FontAwesomeIcons.plus,
-                                              color: provider.selectedFiles.length > 3 ? Colors.grey : Colors.white,
-                                              size: 20,
-                                            ),
-                                          ),
-                                        ),
                                       Expanded(
                                         child: _showVoiceRecorder
                                             ? VoiceRecorderWidget(
@@ -507,8 +405,8 @@ class MPChatPageState extends State<MPChatPage> with AutomaticKeepAliveClientMix
                                                   textAlign: TextAlign.start,
                                                   textAlignVertical: TextAlignVertical.center,
                                                   decoration: const InputDecoration(
-                                                    hintText: 'Ask Anything',
-                                                    hintStyle: TextStyle(fontSize: 16.0, color: Colors.white54),
+                                                    // hintText: 'Ask Anything',
+                                                    // hintStyle: TextStyle(fontSize: 16.0, color: Colors.white54),
                                                     focusedBorder: InputBorder.none,
                                                     enabledBorder: InputBorder.none,
                                                     contentPadding: EdgeInsets.symmetric(horizontal: 8, vertical: 12),
@@ -519,7 +417,7 @@ class MPChatPageState extends State<MPChatPage> with AutomaticKeepAliveClientMix
                                                   keyboardType: TextInputType.multiline,
                                                   textCapitalization: TextCapitalization.sentences,
                                                   style:
-                                                      const TextStyle(fontSize: 16.0, color: Colors.white, height: 1.4),
+                                                      const TextStyle(fontSize: 16.0, color: Color(0xFF4B5563), height: 1.4),
                                                 ),
                                               ),
                                       ),
@@ -533,9 +431,13 @@ class MPChatPageState extends State<MPChatPage> with AutomaticKeepAliveClientMix
                                                   height: 44,
                                                   width: 44,
                                                   alignment: Alignment.center,
+                                                  decoration: BoxDecoration(
+                                                    color: const Color(0xFFF3F4F6),
+                                                    borderRadius: BorderRadius.circular(22),
+                                                  ),
                                                   child: const FaIcon(
                                                     FontAwesomeIcons.xmark,
-                                                    color: Colors.white,
+                                                    color: Color(0xFF4B5563),
                                                     size: 20,
                                                   ),
                                                 ),
@@ -545,9 +447,13 @@ class MPChatPageState extends State<MPChatPage> with AutomaticKeepAliveClientMix
                                                   height: 44,
                                                   width: 44,
                                                   alignment: Alignment.center,
+                                                  decoration: BoxDecoration(
+                                                    color: const Color(0xFFF3F4F6),
+                                                    borderRadius: BorderRadius.circular(22),
+                                                  ),
                                                   child: const FaIcon(
                                                     FontAwesomeIcons.microphone,
-                                                    color: Colors.white,
+                                                    color: Color(0xFF4B5563),
                                                     size: 20,
                                                   ),
                                                 ),
@@ -585,21 +491,12 @@ class MPChatPageState extends State<MPChatPage> with AutomaticKeepAliveClientMix
                                             height: 44,
                                             width: 44,
                                             decoration: BoxDecoration(
-                                              color: canSend ? Colors.white : Colors.grey.withOpacity(0.3),
+                                              color: const Color(0xFFF3F4F6),
                                               borderRadius: BorderRadius.circular(22),
-                                              boxShadow: canSend
-                                                  ? [
-                                                      BoxShadow(
-                                                        color: Colors.black.withOpacity(0.1),
-                                                        blurRadius: 8,
-                                                        offset: const Offset(0, 2),
-                                                      ),
-                                                    ]
-                                                  : [],
                                             ),
-                                            child: Icon(
+                                            child: const Icon(
                                               FontAwesomeIcons.arrowUp,
-                                              color: canSend ? const Color(0xFF35343B) : Colors.grey,
+                                              color:  Color(0xFF4B5563),
                                               size: 20,
                                             ),
                                           ),
@@ -736,34 +633,6 @@ class MPChatPageState extends State<MPChatPage> with AutomaticKeepAliveClientMix
     }
   }
 
-  PreferredSizeWidget _buildAppBar(BuildContext context, MessageProvider provider) {
-    return AppBar(
-      backgroundColor: Theme.of(context).colorScheme.surface,
-      elevation: 0,
-      leading: IconButton(
-        icon: const Icon(Icons.arrow_back, color: Colors.white),
-        onPressed: () => Navigator.of(context).pop(),
-      ),
-      actions: const [],
-      bottom: provider.isLoadingMessages
-          ? PreferredSize(
-              preferredSize: const Size.fromHeight(32),
-              child: Container(
-                width: double.infinity,
-                height: 32,
-                color: Colors.green,
-                child: const Center(
-                  child: Text(
-                    'Syncing messages with server...',
-                    style: TextStyle(color: Colors.white, fontSize: 12),
-                  ),
-                ),
-              ),
-            )
-          : null,
-    );
-  }
-
   void _showAppsMenu(BuildContext ctx, AppProvider appProvider) {
     final renderBox = _appButtonKey.currentContext?.findRenderObject() as RenderBox?;
     if (renderBox == null) return;
@@ -893,41 +762,6 @@ class MPChatPageState extends State<MPChatPage> with AutomaticKeepAliveClientMix
 
     overlay.insert(entry);
     controller.forward();
-  }
-
-  Widget _buildAppSelection(BuildContext context, AppProvider provider) {
-    final messageProvider = Provider.of<MessageProvider>(context, listen: false);
-    var selectedApp = messageProvider.chatApps.firstWhereOrNull((app) => app.id == provider.selectedChatAppId);
-
-    return GestureDetector(
-      key: _appButtonKey,
-      onTap: () {
-        HapticFeedback.mediumImpact();
-        _showAppsMenu(context, provider);
-      },
-      child: Row(
-        crossAxisAlignment: CrossAxisAlignment.center,
-        mainAxisAlignment: MainAxisAlignment.center,
-        mainAxisSize: MainAxisSize.min,
-        children: [
-          selectedApp != null ? _getAppAvatar(selectedApp) : _getOmiAvatar(),
-          const SizedBox(width: 8),
-          Container(
-            constraints: const BoxConstraints(maxWidth: 100),
-            child: Text(
-              selectedApp != null ? selectedApp.getName() : "Omi",
-              style: const TextStyle(color: Colors.white, fontSize: 16),
-              overflow: TextOverflow.fade,
-            ),
-          ),
-          const SizedBox(width: 8),
-          const SizedBox(
-            width: 16,
-            child: Icon(Icons.keyboard_arrow_down, color: Colors.white60, size: 16),
-          ),
-        ],
-      ),
-    );
   }
 
   Widget _getAppAvatar(App app) {
