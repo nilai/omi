e6be9360e0c997ae96ad5186e7180a6c8e4a466d
huangjianjin <49916645+hjj88@users.noreply.github.com>
2025-12-30 16:28:32 +0800

    tmp

--- app/lib/pages/mp_memory/conversation_detail/widgets/mp_tags_card.dart
+++ app/lib/pages/mp_memory/conversation_detail/widgets/mp_tags_card.dart
@@ -23,7 +23,6 @@ class MPTagsCard extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      margin: const EdgeInsets.symmetric(horizontal: 16.0, vertical: 8.0),
       padding: const EdgeInsets.all(20.0),
       width: double.infinity,
       color: Colors.transparent,
