b21f522ccde2d551a28e62006f12238a33e3830a
huang pang <huang@huangdeMac-mini.local>
2026-03-30 15:23:31 +0800

    tmp

--- app/lib/login/mp_user.dart
+++ app/lib/login/mp_user.dart
@@ -25,7 +25,7 @@ class MPUser {
 
   /// 当前用户是否已登录
   // static bool get isLoggedIn => instance.accessToken != null && instance.accessToken!.isNotEmpty;
-  static bool get isLoggedIn => false;
+  static bool get isLoggedIn => true;
 
   /// 清空用户信息（登出）
   void clear() {
