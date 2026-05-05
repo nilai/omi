5ae13a1e199711c1003df98d1d51983064414d02
huang pang <903645893@qq.com>
2026-05-05 08:30:05 +0800

    .

--- app/lib/login/home/mp_login_page.dart
+++ app/lib/login/home/mp_login_page.dart
@@ -170,7 +170,7 @@ class _MPLoginFormBodyState extends State<_MPLoginFormBody> {
                   _AuthTextField(
                     controller: _emailController,
                     hintText: 'your@email.com',
-                    prefixIcon: const Icon(Icons.mail_outline, color: secondTextColor, size: 24),
+                    prefixIcon: const Icon(Icons.mail_outline, color: secondTextColor, size: 20),
                     keyboardType: TextInputType.emailAddress,
                     autocorrect: false,
                     onChanged: cubit.setEmail,
@@ -214,7 +214,7 @@ class _MPLoginFormBodyState extends State<_MPLoginFormBody> {
                   _AuthTextField(
                     controller: _passwordController,
                     hintText: 'Enter your password',
-                    prefixIcon: const Icon(Icons.lock_outline, color: secondTextColor, size: 24),
+                    prefixIcon: const Icon(Icons.lock_outline, color: secondTextColor, size: 20),
                     obscureText: state.obscurePassword,
                     suffixIcon: IconButton(
                       onPressed: cubit.togglePasswordVisible,
